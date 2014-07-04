0c962e2153b86c836f5ae74626cbc8e56bf43753
OdinO4ka <antonerjomin@gmail.com>
2014-07-04 19:01:51 +0400

    add some code

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class JLPTAnswer():
     pass
 
 
+# realisacia
 def json_files_list():
     current_directory = os.getcwd()
     files = os.listdir(current_directory)
@@ -31,21 +32,49 @@ def json_files_list():
     return test
 
 
+# realisacia
 def get_test_type(TestFileName):
     with open(TestFileName) as data_file:
         data = json.load(data_file)
     return data["type"]
 
-
+# realisacia
 def get_test_name(TestFileName):
     with open(TestFileName) as data_file:
         data = json.load(data_file)
     return data["name"]
 
-def get_test_question(TestFileName):
+# realisacia
+def get_text_question(TestFileName, question_num):
     with open(TestFileName) as data_file:
         data = json.load(data_file)
-    return data["questions"][0]
+    return data["questions"][question_num]["text"]
+# realisacia
+def get_test_answers(TestFileName, question_num):
+    with open(TestFileName) as data_file:
+        data = json.load(data_file)
+    i = 0
+    a = []
+    while (i<len(data["questions"][question_num]["answers"])):
+        a.append(data["questions"][question_num]["answers"][i]["answer"])
+        i += 1
+    return a
+
+
+# realisacia
+right_answer_counter = 0
+
+def isRightAnsw(TestFileName, question_num, answer):
+    global right_answer_counter
+    with open(TestFileName) as data_file:
+        data = json.load(data_file)
+    i = 0
+    while (i<len(data["questions"][question_num]["answers"])):
+        if data["questions"][question_num]["answers"][i]["isright"] == "true":
+            right_answer = data["questions"][question_num]["answers"][i]["answer"]
+            if right_answer == answer:
+                right_answer_counter = right_answer_counter + 1
+        i += 1
 
 all_type_list = []
 counter = 0
@@ -56,6 +85,8 @@ while (counter < len(json_files_list())):
 
 # external services
 
+
+
 # console work
 print("You can choose between: {}".format(all_type_list))
 line = input("Input your test type:")
@@ -63,12 +94,20 @@ line = input("Input your test type:")
 # work logic
 if line == "full":
     print("You choose {}".format(get_test_name(json_files_list()[0])))
-    print(get_test_question(json_files_list()[0]))
+    print(get_text_question(json_files_list()[0], 0))
+    print(get_test_answers(json_files_list()[0], 0))
+    answer = input()
+    isRightAnsw(json_files_list()[0], 0, answer)
+    print(right_answer_counter)
+    print()
 elif line == "N2":
     print("You choose {}".format(get_test_name(json_files_list()[1])))
 elif line == "N5":
     print("You choose {}".format(get_test_name(json_files_list()[2])))
 
 print("Prepare for battle")
+
+
+
 
 input()
